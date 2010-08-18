588aa47aaad9677d351211fec759eb38563a4aa2
Siddhant Sanyam <siddhant3s@gmail.com>
2010-08-19 03:16:44 +0530

    Added array_delete

--- q1.c
+++ q1.c
@@ -2,7 +2,7 @@
 1. Suppose that an ordered list of numbers is implemented by means of an array, Write a C program using separate functions to 
    i.      Insert a number
    ii.     Delete an element
-   iii.    Reverse the lis
+   iii.    Reverse the list
 */
 #include <stdio.h>
 
@@ -26,19 +26,40 @@ with the error printed on stderr*/
       return 0;
     }
   int *p=a+position;//the pointer to the location where the new element will get inserted
-  a=a+size;//a points to one past the last element of original array
+  a+=size;//a points to one past the last element of original array
   while(a>p)
     *(a--)=*(a-1);
   *a=element;
   return 1;
 }
+
+int array_delete(int *a, size_t size, size_t position)
+/*Removes the element a[position] from the array. The `size' denotes the  number of
+  elements in the array before the removal. Returned zero if the deletion failed
+else returns a positive integer.*/
+{
+  if (position >=size)
+    {
+      fprintf(stderr,"\narray_delete: The specified position %i is not less than the"
+	      " size of the array which is %i\n",position,size);
+      return 0;
+    }
+  int *last=a+size-1;// points to the last element
+  a+=position;//a now points to the position of the deltetion
+  while(a<last) *a++=*(a+1);
+  return 1;
+}
+
 int main()
 {
   int a[10]={5,4,1,2,5};
   size_t s=(sizeof a)/(sizeof *a);
   //printf("Size:%i",s);
   array_print(a,s);
-  array_insert(a,5,121,5);
-  array_print(a,s);
+  array_insert(a,5,7,3);
+  printf("\n");array_print(a,s);
+  array_delete(a,6,2);
+  printf("\n");array_print(a,s);
+  
   return 0;
 }
