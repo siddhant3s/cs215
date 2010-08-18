c167a36a8fc80e0582cc358a06d8db496905d35d
Siddhant Sanyam <siddhant3s@gmail.com>
2010-08-19 00:37:11 +0530

    First Commit

---
+++ q1.c
@@ -0,0 +1,11 @@
+/*
+1. Suppose that an ordered list of numbers is implemented by means of an array, Write a C program using separate functions to 
+   i.      Insert a number
+   ii.     Delete an element
+   iii.    Reverse the lis
+*/
+#include <stdio.h>
+int array_insert(int a[], size_t size, int element, size_t position)
+/* Inserts the element
+{
+  if (
